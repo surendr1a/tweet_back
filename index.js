cee9073245f2b289300374c2baee61f507d0a434
test <gyandev081@gmail.com>
2025-08-14 08:58:23 +0530

    again doing modifications for the working of puppeteer or whatever

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post("/generate", async (req, res) => {
     res.status(500).json({ error: "Failed to capture tweet image" });
   }
 });
-
-app.listen(5000, () => {
-  console.log("✅ Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
 });
